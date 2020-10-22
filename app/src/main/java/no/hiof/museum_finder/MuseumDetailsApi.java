27376d37dd5522dbbef2bd6e2013dbd05dce4ab3
blend-i <blendim@hiof.no>
2020-10-22 03:22:24 +0200

    la til logikk på hva som skjer hvis lokasjonen er null og den blir oppdatert.

--- app/src/main/java/no/hiof/museum_finder/MuseumDetailsApi.java
+++ app/src/main/java/no/hiof/museum_finder/MuseumDetailsApi.java
@@ -188,6 +188,24 @@ public class MuseumDetailsApi extends AppCompatActivity {
                                 }
                                 //update location
                                 lastKnownLocation = locationResult.getLastLocation();
+
+
+                                Geocoder geocoder;
+                                List<Address> addresses = null;
+                                geocoder = new Geocoder(MuseumDetailsApi.this, Locale.getDefault());
+
+                                try {
+                                    addresses = geocoder.getFromLocation(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude(), 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+                                String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+                                String city = addresses.get(0).getLocality();
+                                String state = addresses.get(0).getAdminArea();
+                                String country = addresses.get(0).getCountryName();
+                                String postalCode = addresses.get(0).getPostalCode();
+                                String knownName = addresses.get(0).getFeatureName(); // Only if available else return NULL
                             }
                         };
                     }
