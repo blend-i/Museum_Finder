4e7cd6bd0061e09e6d403876b3218d92841bec2e
mienna91 <kristomi@hiof.no>
2020-10-30 19:58:37 +0100

    implementert bottomnavigation

--- app/src/main/java/no/hiof/museum_finder/MainActivity.java
+++ app/src/main/java/no/hiof/museum_finder/MainActivity.java
@@ -1,11 +1,18 @@
 package no.hiof.museum_finder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        NavController controller = Navigation.findNavController(this, R.id.fragment);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_NavBar);
+        NavigationUI.setupWithNavController(bottomNavigationView, controller);
     }
     public MainActivity() {
 
