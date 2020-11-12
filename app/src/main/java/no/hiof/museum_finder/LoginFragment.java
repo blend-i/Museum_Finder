6505400bb5c8935bf741a044263d7d1f6c80f02d
mienna91 <kristomi@hiof.no>
2020-11-12 20:02:02 +0100

    Fiksa skriving av kontodetaljer til database og henter kontodetaljer i profil

--- app/src/main/java/no/hiof/museum_finder/LoginFragment.java
+++ app/src/main/java/no/hiof/museum_finder/LoginFragment.java
@@ -22,8 +22,13 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Objects;
+
+import no.hiof.museum_finder.model.Account;
 
 import static android.content.ContentValues.TAG;
 import static java.util.Objects.*;
@@ -32,6 +37,8 @@ public class LoginFragment extends Fragment {
 
     private GoogleSignInClient mGoogleSignInClient;
     private SignInButton signInButton;
+    private FirebaseFirestore firestoreDb;
+    private CollectionReference collectionReference;
 
 
 
@@ -127,6 +134,7 @@ public class LoginFragment extends Fragment {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
 
+            addAccountToDb(new Account(account.getGivenName(), account.getFamilyName(), account.getEmail(), account.getPhotoUrl().toString()));
             // Signed in successfully, show authenticated UI.
             updateUI(account);
         } catch (ApiException e) {
@@ -137,6 +145,11 @@ public class LoginFragment extends Fragment {
         }
     }
 
+    private void addAccountToDb(Account account) {
+        firestoreDb = FirebaseFirestore.getInstance();
+        collectionReference = firestoreDb.collection("account");
+        collectionReference.add(account);
+    }
 
 
         //button.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_homeFragment));
--- app/src/main/java/no/hiof/museum_finder/ProfileFragment.java
+++ app/src/main/java/no/hiof/museum_finder/ProfileFragment.java
@@ -1,6 +1,7 @@
 package no.hiof.museum_finder;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,16 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import no.hiof.museum_finder.model.Account;
+import no.hiof.museum_finder.model.Museum;
+
+import static android.content.ContentValues.TAG;
 
 public class ProfileFragment extends Fragment {
 
@@ -30,8 +41,8 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        nameTextView = view.findViewById(R.id.nameTextView);
+        getAccountInformationFromDb("account", view);
+        /*nameTextView = view.findViewById(R.id.nameTextView);
         emailTextView = view.findViewById(R.id.eMailTextView);
         profilePictureImageView = view.findViewById(R.id.profileImageView);
 
@@ -54,6 +65,46 @@ public class ProfileFragment extends Fragment {
                     .into(profilePictureImageView);
         } else {
             profilePictureImageView.setImageResource(R.drawable.kon_tiki_museet);
-        }
+        }*/
+    }
+
+    private void getAccountInformationFromDb(String documentReference, View view) {
+        FirebaseFirestore firestoreDb = FirebaseFirestore.getInstance();
+        final DocumentReference accountDocumentReference = firestoreDb.collection(documentReference).document("ecKnqpM3RS85wIYIuJAK");
+
+        nameTextView = view.findViewById(R.id.nameTextView);
+        emailTextView = view.findViewById(R.id.eMailTextView);
+        profilePictureImageView = view.findViewById(R.id.profileImageView);
+
+        accountDocumentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    Account account = documentSnapshot.toObject(Account.class);
+                    //account.setUid(documentSnapshot.getId());
+
+                    StringBuilder fullName = new StringBuilder();
+                    fullName.append(account.getFirstName());
+                    fullName.append(" ");
+                    fullName.append(account.getLastName());
+
+                    nameTextView.setText(fullName.toString());
+                    emailTextView.setText(account.geteMail());
+
+                    if(account.getProfilePictureUrl() != null && !account.getProfilePictureUrl().equals("")) {
+                        Glide.with(profilePictureImageView.getContext())
+                                .load(account.getProfilePictureUrl())
+                                .into(profilePictureImageView);
+                    } else {
+                        profilePictureImageView.setImageResource(R.drawable.kon_tiki_museet);
+                    }
+                }
+                else
+                {
+                    Log.d(TAG, "Get failed with", task.getException());
+                }
+            }
+        });
     }
 }
