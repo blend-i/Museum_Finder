6aed1cc866719e552ca988ebe1040a7accf30b14
mienna91 <kristomi@hiof.no>
2020-11-27 09:38:12 +0100

    nu fungerer  mappet som det skal i vanlig fragment

--- app/src/main/java/no/hiof/museum_finder/MapFragment.java
+++ app/src/main/java/no/hiof/museum_finder/MapFragment.java
@@ -1,27 +1,131 @@
 package no.hiof.museum_finder;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-public class MapFragment extends FragmentActivity implements OnMapReadyCallback {
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
 
+public class MapFragment extends Fragment {
+
+    private final int PERMISSION_LOCATION_ID = 1;
+    private SupportMapFragment mapFragment;
+    private GoogleMap map;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    double currentLat = 0, currentLong= 0;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        //mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.mapFragment);
+
+        //
+
+        //mapFragment = SupportMapFragment.newInstance();
+        //getChildFragmentManager().beginTransaction().setReorderingAllowed(true).add(R.id.map, mapFragment, "tag").commit();
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        mapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        Log.d("onCreateView", "I onCreateView");
+
+        String placeType = "museum";
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext());
+
+
+        if(EasyPermissions.hasPermissions(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)) {
+            getCurrentLocation();
+            Log.d("HAS PERMISSION", "HAR PERMISSION");
+        } else {
+            Log.d("HAS NOT PERMISSION", "HAR PERMISSION");
+            EasyPermissions.requestPermissions(this, "Access fine location needed to get my location", PERMISSION_LOCATION_ID, Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        Log.d("onCreate", "I onCreate");
+
+        return view;
+    }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+
+
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                        new LatLng(currentLat, currentLong), 10
+                ));
+            }
+        });
     }
+
+    @SuppressLint("MissingPermission")
+    @AfterPermissionGranted(PERMISSION_LOCATION_ID)
+    private void getCurrentLocation() {
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location != null) {
+                    currentLat = location.getLatitude();
+                    currentLong = location.getLongitude();
+
+                    mapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(GoogleMap googleMap) {
+                            map = googleMap;
+
+                            map.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(currentLat, currentLong), 10
+                            ));
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 44) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation();
+            }
+        }
+    }
+
+
+
 }
