9b61ea0de8cd3777c38a3f41e321d087ea7deaa9
blend-i <55741869+blend-i@users.noreply.github.com>
2020-11-29 22:07:10 +0100

    ordnet funksjonalitet som viser hvor langt unna museum er på cardviewet i HomeFragmentApi recycleview

--- app/src/main/java/no/hiof/museum_finder/MuseumDetailFragment.java
+++ app/src/main/java/no/hiof/museum_finder/MuseumDetailFragment.java
@@ -1,25 +1,18 @@
 package no.hiof.museum_finder;
 
-import android.app.Activity;
-import android.app.SharedElementCallback;
 import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 
-import android.os.Handler;
-import android.os.Looper;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.BounceInterpolator;
 import android.view.animation.ScaleAnimation;
-import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.RatingBar;
@@ -32,25 +25,15 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.transition.MaterialContainerTransform;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.SetOptions;
-
-import java.util.ArrayList;
-import java.util.EventListener;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import no.hiof.museum_finder.model.Museum;
 
-import static android.content.ContentValues.TAG;
 
 public class MuseumDetailFragment extends Fragment {
-    public static final String MUSEUM_UID = "museum_uid";
     private TextView museumTitle;
     private TextView museumDescription;
     private ImageView museumImage;
@@ -61,7 +44,6 @@ public class MuseumDetailFragment extends Fragment {
     private Museum museum;
     private FirebaseFirestore fireStoreDb;
     private ToggleButton favourite;
-
     private CollectionReference bucketCollectionReference;
 
     public MuseumDetailFragment() {
@@ -88,7 +70,7 @@ public class MuseumDetailFragment extends Fragment {
         assert arguments != null;
         MuseumDetailFragmentArgs args = MuseumDetailFragmentArgs.fromBundle(arguments);
 
-        museum = new Museum(args.getTitle(), "Museum description", args.getLocation(), args.getOpeningHours(),args.getPhotoUrl(), args.getPlaceId(), args.getRating());
+        museum = new Museum(args.getTitle(), "Museum description", args.getLocation(), args.getOpeningHours(), args.getPhotoUrl(), args.getPlaceId(), args.getRating());
         return inflater.inflate(R.layout.fragment_museum_detail, container, false);
     }
 
@@ -102,26 +84,15 @@ public class MuseumDetailFragment extends Fragment {
         museumOpeningHours = view.findViewById(R.id.openingHoursTextView);
         museumRating = view.findViewById(R.id.ratingBarDetail);
 
-        /*museumTitle.setVisibility(View.INVISIBLE);
-        museumDescription.setVisibility(View.INVISIBLE);
-        museumImage.setVisibility(View.INVISIBLE);
-        museumLocation.setVisibility(View.INVISIBLE);
-        museumOpeningHours.setVisibility(View.INVISIBLE);*/
-
         fireStoreDb = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        Bundle arguments = getArguments();
-        assert arguments != null;
-        MuseumDetailFragmentArgs args = MuseumDetailFragmentArgs.fromBundle(arguments);
         museumTitle.setText(museum.getTitle());
         //museumDescription.setText(args.getDescription());
         museumOpeningHours.setText(museum.getOpen().equals("true") ? "Open" : "Closed");
         museumLocation.setText(museum.getLocation());
         museumRating.setRating(Float.parseFloat(museum.getRating()));
 
-        System.out.println("ARGSDATA: " + args.getTitle() + " " + args.getOpeningHours() + " " + args.getPhotoUrl());
-
-        //args.getPosterUrl();
         if (!museum.getPhoto().isEmpty()) {
 
             String url = "https://maps.googleapis.com/maps/api/place/photo" +
@@ -134,76 +105,10 @@ public class MuseumDetailFragment extends Fragment {
                     .into(museumImage);
         }
 
-        System.out.println("MUSEUMID " + museum.getPlaceId());
-        /*final String museumUid = args.getId();
-
-       firebaseAuth = FirebaseAuth.getInstance();
-
-       FirebaseUser user = firebaseAuth.getCurrentUser();
-
-        FirebaseFirestore firestoreDb = FirebaseFirestore.getInstance();
-        final DocumentReference museumCollectionReference = firestoreDb.collection("museum").document(museumUid);
-
-        museumCollectionReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-
-                if (task.isSuccessful()) {
-
-                    final Handler handler = new Handler(Looper.getMainLooper());
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            //Do something after 100ms
-
-                            museumTitle.setVisibility(View.VISIBLE);
-                            museumDescription.setVisibility(View.VISIBLE);
-                            museumImage.setVisibility(View.VISIBLE);
-                            museumLocation.setVisibility(View.VISIBLE);
-                            museumOpeningHours.setVisibility(View.VISIBLE);
-                        }
-                    }, 250);
-
-                    DocumentSnapshot documentSnapshot = task.getResult();
-                    museum = documentSnapshot.toObject(Museum.class);
-                    museum.setUid(documentSnapshot.getId());
-
-                    StringBuilder openingHours = new StringBuilder();
-                    openingHours.append("Opening hours: ");
-                    openingHours.append(museum.getOpeningHours());
-
-                    StringBuilder location = new StringBuilder();
-                    location.append("Location: ");
-                    location.append(museum.getLocation());
-
-                    museumTitle.setText(museum.getTitle());
-                    museumDescription.setText(museum.getDescription());
-                    museumLocation.setText(location.toString());
-                    museumOpeningHours.setText(openingHours.toString());
-
-                    if (museum.getPosterUrl() != null && !museum.getPosterUrl().isEmpty()) {
-                        Glide.with(museumImage.getContext())
-                                .load(museum.getPosterUrl())
-                                .into(museumImage);
-                    }
-
-                    checkIfMusuemExistsInBucketList(museumUid);
-                }
-                else
-                {
-                    Log.d(TAG, "Get failed with", task.getException());
-                }
-            }
-        });*/
-
-        final String museumUid = args.getPlaceId();
-
-
-        firebaseAuth = FirebaseAuth.getInstance();
+        final String museumPlaceId = museum.getPlaceId();
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
         bucketCollectionReference = fireStoreDb.collection("account").document(user.getUid()).collection("bucketList");
-
 
         favourite = getView().findViewById(R.id.button_favorite);
         final ScaleAnimation scaleAnimation = new ScaleAnimation(0.7f, 1.0f, 0.7f, 1.0f, Animation.RELATIVE_TO_SELF, 0.7f, Animation.RELATIVE_TO_SELF, 0.7f);
@@ -211,23 +116,21 @@ public class MuseumDetailFragment extends Fragment {
         BounceInterpolator bounceInterpolator = new BounceInterpolator();
         scaleAnimation.setInterpolator(bounceInterpolator);
 
-
         favourite.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
                 //animation
 
                 if (isChecked) {
-                    setCheckedBucketList(museumUid, true);
+                    setCheckedBucketList(museumPlaceId, true);
                     System.out.println("UNCHECKED");
                 } else {
-                    setCheckedBucketList(museumUid, false);
+                    setCheckedBucketList(museumPlaceId, false);
                 }
                 compoundButton.startAnimation(scaleAnimation);
             }
         });
-
-        checkIfMusuemExistsInBucketList(args.getPlaceId());
+        checkIfMusuemExistsInBucketList(museum.getPlaceId());
     }
 
 
@@ -236,28 +139,21 @@ public class MuseumDetailFragment extends Fragment {
         bucketListSpecificMuseumReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if(task.isSuccessful()) {
+                if (task.isSuccessful()) {
                     DocumentSnapshot documentSnapshot = task.getResult();
 
                     if (documentSnapshot.exists()) {
                         museum = documentSnapshot.toObject(Museum.class);
                         museum.setFavorite(bool);
-
-
-                        if(!museum.isFavorite()){
+                        if (!museum.isFavorite()) {
                             bucketCollectionReference.document(museumId).delete();
                         }
                     } else {
                         bucketCollectionReference.document(museumId).set(museum);
-
-                        //bucketListSpecificMuseumReference.set(museum);
-                        System.out.println("ADDED TO BUCKETLIST");
                     }
                 }
             }
         });
-
-
     }
 
     private void checkIfMusuemExistsInBucketList(final String museumId) {
@@ -265,10 +161,10 @@ public class MuseumDetailFragment extends Fragment {
         bucketListSpecificMuseumReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if(task.isSuccessful()) {
+                if (task.isSuccessful()) {
                     DocumentSnapshot documentSnapshot = task.getResult();
 
-                    if(documentSnapshot.exists()) {
+                    if (documentSnapshot.exists()) {
                         favourite.setChecked(true);
                         System.out.println("DOCUMENT EXISTS IN BUCKETLIST");
                     } else {
--- app/src/main/java/no/hiof/museum_finder/adapter3/MuseumRecyclerAdapterApi.java
+++ app/src/main/java/no/hiof/museum_finder/adapter3/MuseumRecyclerAdapterApi.java
@@ -1,6 +1,8 @@
 package no.hiof.museum_finder.adapter3;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.location.Location;
 import android.os.Build;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,23 +17,27 @@ import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.List;
 
 import no.hiof.museum_finder.CardViewClickManager;
 import no.hiof.museum_finder.R;
 import no.hiof.museum_finder.model.Museum;
-
-/*
-Det som skjer her:
-
-Vi har en konstruktør som tar imot context og en liste med dyr.
-Fra context så lager vi en inflater. Denne inflateren bruker vi for å få et view basert på vår xml.
-Det viewet bruker vi til å opprette new Viewholder slik at hver gang recyclerView finner ut at den trenger ny viewholder så sier den til adapteren "lag en ny viewholder til meg"
-Da får viewholder en instant av lista i form av view og da kan hente ut de enkle viewene.
-
- */
 
 public class MuseumRecyclerAdapterApi extends RecyclerView.Adapter<MuseumRecyclerAdapterApi.MuseumViewHolderApi> {
     private static final String TAG = MuseumRecyclerAdapterApi.class.getSimpleName();
@@ -82,6 +88,11 @@ public class MuseumRecyclerAdapterApi extends RecyclerView.Adapter<MuseumRecycle
         private TextView descriptionTextView;
         private Button thumbnailButton;
         private RatingBar ratingBar;
+        private TextView distance;
+        private RequestQueue requestQueue;
+        private FusedLocationProviderClient fusedLocationProviderClient;
+        private double originLat;
+        private double originLng;
 
         public MuseumViewHolderApi(@NonNull final View itemView) {
             super(itemView);
@@ -89,6 +100,10 @@ public class MuseumRecyclerAdapterApi extends RecyclerView.Adapter<MuseumRecycle
             thumbnailimageView = itemView.findViewById(R.id.thumbnailimageView);
             descriptionTextView = itemView.findViewById(R.id.descriptionTextView);
             ratingBar = itemView.findViewById(R.id.ratingBarApi);
+            distance = itemView.findViewById(R.id.distanceTextView);
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(itemView.getContext());
+            originLat = 0;
+            originLng = 0;
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -99,36 +114,79 @@ public class MuseumRecyclerAdapterApi extends RecyclerView.Adapter<MuseumRecycle
         }
 
         public void setMuseum(final Museum museumToDisplay) {
-            String posterUrl = museumToDisplay.getPhoto();
+            requestQueue = Volley.newRequestQueue(itemView.getContext());
 
+            String posterUrl = museumToDisplay.getPhoto();
             String rating = museumToDisplay.getRating();
 
-            String url = "https://maps.googleapis.com/maps/api/place/photo" +
+            double lat = museumToDisplay.getLat();
+            double lng = museumToDisplay.getLng();
+
+            String photoUrl = "https://maps.googleapis.com/maps/api/place/photo" +
                     "?maxwidth=" + 400 +
                     "&photoreference=" + posterUrl +
-                    "&key=AIzaSyCis2iHvAD0nBpKigxJAHA0CVGo_vq88nc";
+                    "&key=" + itemView.getResources().getString(R.string.maps_api_key);
 
-            System.out.println("POSTERURL FRA ADAPTER: " + posterUrl);
+            //Suppressing here
+            @SuppressLint("MissingPermission") Task<Location> task = fusedLocationProviderClient.getLastLocation();
+
+            task.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    originLat = location.getLatitude();
+                    originLng = location.getLongitude();
+
+                    jsonParseAndDisplayDistanceInKm(originLat, originLng, lat, lng);
+                }
+            });
 
             //sets the title
             thumbnailTextView.setText(museumToDisplay.getTitle());
 
             //sets the photo of museum in cardview
-            if(posterUrl != null && !posterUrl.equals("")) {
+            if (posterUrl != null && !posterUrl.equals("")) {
                 Glide.with(thumbnailimageView.getContext())
-                        .load(url)
+                        .load(photoUrl)
                         .into(thumbnailimageView);
             }
 
             //sets if museum open or closed
-            if(museumToDisplay.getOpen().equals("true")) {
+            if (museumToDisplay.getOpen().equals("true")) {
                 descriptionTextView.setText("Open");
             } else {
                 descriptionTextView.setText("Closed");
             }
 
             ratingBar.setRating(Float.parseFloat(rating));
+        }
 
+        private void jsonParseAndDisplayDistanceInKm(double origin_lat, double origin_lng, double destination_lat, double destination_lng) {
+            String url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins="+origin_lat+","+origin_lng+"&destinations="+ destination_lat + "," + destination_lng+"&key="+itemView.getResources().getString(R.string.maps_api_key);
+
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        JSONArray jsonArray = response.getJSONArray("rows");
+                        JSONObject elements = jsonArray.getJSONObject(0);
+                        JSONArray elementsArray = elements.getJSONArray("elements");
+                        JSONObject distanceAndDuration = elementsArray.getJSONObject(0);
+                        String meters = distanceAndDuration.getJSONObject("distance").getString("value");
+
+                        distance.setText((Integer.parseInt(meters) / 1000) + " km away");
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        System.out.println("ERROR I ADAPTER");
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    error.printStackTrace();
+                }
+            });
+            requestQueue.add(request);
         }
     }
 }
